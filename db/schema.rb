006d20f9fe6359ecef14795044b4773de9245d80
Hung Nguyen <hungtn95@gmail.com>
2017-01-06 05:04:19 -0600

    Add Course and Lesson model

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170106102820) do
+ActiveRecord::Schema.define(version: 20170106105326) do
 
   create_table "active_admin_comments", force: :cascade do |t|
     t.string   "namespace"
@@ -45,6 +45,26 @@ ActiveRecord::Schema.define(version: 20170106102820) do
 
   add_index "admin_users", ["email"], name: "index_admin_users_on_email", unique: true
   add_index "admin_users", ["reset_password_token"], name: "index_admin_users_on_reset_password_token", unique: true
+
+  create_table "courses", force: :cascade do |t|
+    t.string   "name"
+    t.text     "content"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "lessons", force: :cascade do |t|
+    t.string   "title"
+    t.text     "note"
+    t.string   "video"
+    t.boolean  "header",     default: false, null: false
+    t.string   "tag"
+    t.integer  "course_id"
+    t.datetime "created_at",                 null: false
+    t.datetime "updated_at",                 null: false
+  end
+
+  add_index "lessons", ["course_id"], name: "index_lessons_on_course_id"
 
   create_table "users", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
