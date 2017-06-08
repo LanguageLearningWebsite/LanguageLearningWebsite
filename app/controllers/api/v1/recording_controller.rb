88b6aa8944f8f5a499244bcf4d92de0efb2e50ad
Hung Nguyen <hungtn95@gmail.com>
2017-06-08 02:57:06 -0500

    New recording view

--- app/controllers/api/v1/recording_controller.rb
+++ app/controllers/api/v1/recording_controller.rb
@@ -4,6 +4,8 @@ class Api::V1::RecordingController < ApplicationController
 
   def new
     Recording.create!(:url => params[:url], :user_id => params[:user_id], :recording_list_id => params[:recording_list_id])
-    render :nothing => true
+    component = RecordingList.find(params[:recording_list_id]).component
+    flash[:notice] = "Recording submitted!"
+    redirect_to course_lesson_component_path(component.lesson.course, component.lesson, component)
   end
 end
