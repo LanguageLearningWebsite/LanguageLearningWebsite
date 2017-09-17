db82719f71ace7b0510ba6eb12ffee8d2880f8f0
muoivpham <m.v.pham@tcu.edu>
2017-09-17 01:41:53 -0500

    change design

--- db/schema.rb
+++ db/schema.rb
@@ -12,7 +12,7 @@
 # It's strongly recommended that you check this file into your version control system.
 
 ActiveRecord::Schema.define(version: 20170806073515) do
-  
+
   create_table "active_admin_comments", force: :cascade do |t|
     t.string   "namespace"
     t.text     "body"
